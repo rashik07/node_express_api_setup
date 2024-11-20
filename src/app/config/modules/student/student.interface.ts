4d5f40d628b8120ec96458c6d8a078f164ef44af
rashik07 <saif67090707@gmail.com>
2024-11-21 00:04:50 +0600

    update student interface

---
+++ src/app/config/modules/student/student.interface.ts
@@ -0,0 +1,37 @@
+
+import { Schema, model, connect } from 'mongoose';
+
+export type Guardian = {
+  fatherName: string;
+  fatherOccupation: string;
+  fatherContactNo: string;
+  motherName: string;
+  motherOccupation: string;
+  motherContactNo: string;
+};
+export type UserName={
+    firstName: string; middleName: string; lastName: string 
+}
+
+export type LocalGuardian={
+    guardianName: string;
+    guardianOccupation: string;
+    guardianContactNo: string;
+}
+export type Student = {
+  id: string;
+  name: UserName;
+  gender: 'male' | 'female';
+  dateOfBirth: string;
+  email: string;
+  contactNumber: string;
+  bloodGroup?: 'A+' | 'A-' | 'o+' | 'o-';
+  presentAddress: string;
+  permanentAddress: string;
+  guardian: Guardian;
+  localGuardian:LocalGuardian;
+  profileImg:string;
+  avatar?: string;
+};
+
+
