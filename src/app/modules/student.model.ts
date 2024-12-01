48a72de6537be94f971ac974456370c902f812ab
rashik07 <saif67090707@gmail.com>
2024-12-01 17:15:19 +0600

    1/12/2024

--- src/app/modules/student.model.ts
+++ src/app/modules/student.model.ts
@@ -5,6 +5,8 @@ import {
   Student,
   UserName,
 } from './student/student.interface';
+import validator from 'validator';
+import isEmail from 'validator/lib/isEmail';
 
 // Guardian Subdocument Schema
 const GuardianSchema = new Schema<Guardian>({
@@ -18,9 +20,28 @@ const GuardianSchema = new Schema<Guardian>({
 
 // UserName Subdocument Schema
 const UserNameSchema = new Schema<UserName>({
-  firstName: { type: String, required: true },
+  firstName: {
+    type: String,
+    required: [true, 'First Name is required'],
+    trim: true,
+    maxLength: [20, 'first Name can not be more than 20'],
+    validate: {
+      validator: function (value) {
+        const firstNameStr = value.charAt(0).toUpperCase() + value.slice(1);
+        return firstNameStr === value;
+      },
+      message: '{VALUE} is not a capitaliz formate',
+    },
+  },
   middleName: { type: String, required: false },
-  lastName: { type: String, required: true },
+  lastName: {
+    type: String,
+    required: true,
+    validate: {
+      validator: (value: string) => validator.isAlpha(value),
+      message: '{VALUE} is not valid',
+    },
+  },
 });
 
 // Local Guardian Subdocument Schema
@@ -32,11 +53,31 @@ const LocalGuardianSchema = new Schema<LocalGuardian>({
 
 // 2. Create a Schema corresponding to the document interface.
 const studentSchema = new Schema<Student>({
-  id: { type: String },
+  id: { type: String, required: true, unique: true },
   name: { type: UserNameSchema, required: true },
-  gender: { type: String, enum: ['male', 'female'], required: true },
+  gender: {
+    type: String,
+    enum: {
+      values: ['male', 'female'],
+      message: 'error messager for gender',
+    },
+    required: true,
+  },
   dateOfBirth: { type: String },
-  email: { type: String, required: true },
+  email: {
+    type: String,
+    required: [true, 'Enail is required'],
+    unique: true,
+    validate: {
+      validator: (value: string) => validator.isEmail(value),
+      message: '{VALUE} is mot a valid email',
+    },
+    //   validate:{
+    //   validator: (value: string)=> validator,isEmail(value),
+    //   message:'{VALUE} is not a valid email type'
+
+    // },
+  },
   contactNumber: { type: String, required: true },
   bloodGroup: { type: String, enum: ['A+', 'A-', 'O+', 'O-', 'B+', 'B-'] },
   presentAddress: { type: String, required: true },
